53aa428bdf873bc833f622679fb17db7cfbd1641
Will Gu <willgu29@gmail.com>
2017-12-11 00:14:18 -0800

    try mail

--- server/api/mails.js
+++ server/api/mails.js
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import Mail from './models/Mail.js'
 
+import nodemailer from 'nodemailer'
+
 const router = Router()
 
 
@@ -22,6 +24,36 @@ router.get('/mails/tracking.gif', function (req, res, next) {
   console.log('Hello, this is an opened email!')
   // update the mail id
   res.send('none')
+})
+
+router.get('/mails/test', function (req, res, next) {
+  console.log('sending test mail!')
+
+  let transporter = nodemailer.createTransport({
+      host: 'smtp.localhost',
+      port: 465,
+      secure: true, // secure:true for port 465, secure:false for port 587
+  })
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Will Gu" <will@penguinjeffrey.com>', // sender address
+      to: 'willgu29@gmail.com', // list of receivers
+      subject: 'Hello', // Subject line
+      text: 'Hello world!', // plain text body
+      html: '<b>Hello world!</b>' // html body
+  }
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error)
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response)
+  })
+  res.json("hi")
+
+
 })
 
 export default router
