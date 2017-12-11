ddfa82241168287e111b538b2ae42e742c7cba2a
Will Gu <willgu29@gmail.com>
2017-12-10 19:44:12 -0800

    port 3001

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
   <section class="container">
-    <img id="tracking" src="https://www.penguinjeffrey.com/api/mails/tracking" alt="Penguin Jeffrey Logo" class="logo" />
-
+    <img id="tracking" src="https://mail.penguinjeffrey.com/api/mails/tracking" alt="Penguin Jeffrey Logo" class="logo" />
+    <p>Hello</p>
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
 
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data }
-  },
   head () {
     return {
       title: 'Users'
--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import users from './users'
+import mails from './mails'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
+router.use(mails)
 
 export default router
