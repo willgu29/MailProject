5e335ae8fd2f748ad9598168ecbbbddae2beb743
Will Gu <willgu29@gmail.com>
2018-01-09 12:29:04 -0800

    gmail redirect route

--- server/api/index.js
+++ server/api/index.js
@@ -11,4 +11,12 @@ router.use(users)
 router.use(mails)
 router.use(campaigns)
 
+router.get('/redirect-gmail', function (req, res, next) {
+  var code = req.query.code;
+  if (! code) { return res.sendStatus(404) }
+  console.log(code);
+  res.send(code)
+
+})
+
 export default router
